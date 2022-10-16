4df5fe27ef6ded0fb5071e44f3a317486dc15cb1
chenzhizsqq <chenzhi@qq.com>
2022-10-16 17:47:40 +0900

    Initial Commit

---
+++ AVKit_play/AVKit_playApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVKit_playApp.swift
+//  AVKit_play
+//
+//  Created by chenzhizs on 2022/10/16.
+//
+
+import SwiftUI
+
+@main
+struct AVKit_playApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AVKit_play/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AVKit_play
+//
+//  Created by chenzhizs on 2022/10/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
